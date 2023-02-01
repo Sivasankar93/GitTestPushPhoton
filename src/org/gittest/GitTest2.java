97c05bfcd95a2189c2b2530383840cf0b0870a63
Sivasankar93 <sivasankar2658@gmail.com>
2023-02-01 15:33:38 +0530

    Person A work

--- src/org/gittest/GitTest2.java
+++ src/org/gittest/GitTest2.java
@@ -4,5 +4,7 @@ public class GitTest2 {
 public static void main(String[] args) {
 	String s ="Sankar";
 	System.out.println(s);
+	boolean contains = s.contains("S");
+	System.out.println(contains);
 }
 }
